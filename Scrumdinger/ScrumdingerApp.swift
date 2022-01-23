d81101ee362f51a57ad197893fcd9e56d2a21c46
EvgeniyKostin <kostinen@icloud.com>
2022-01-23 11:43:06 +0300

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Evgeniy Kostin on 23.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
